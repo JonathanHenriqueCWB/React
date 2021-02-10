bee497d930860c24ec4c489f9ed9874949a44cf3
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2021-02-10 13:55:04 -0300

    Estrutura do projeto function component

---
+++ CursoUdemy/fundamentos-react/src/components/basicos/Primeiro.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Primeiro (){
+    return 'Primeiro componente'
+}
+
+export default Primeiro
---
+++ CursoUdemy/fundamentos-react/src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Primeiro from './components/basicos/Primeiro'
+
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
