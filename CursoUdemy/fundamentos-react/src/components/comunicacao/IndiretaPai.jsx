4ef101832e9d289d4ec1ec44c2a983da36dc36c1
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2021-02-18 16:56:19 -0300

    Componente com estado (state)

--- CursoUdemy/fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ CursoUdemy/fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IndiretoFilho from './IndiretaFilho'
 
 const IndiretaPai = () => {
 
+    const [nome, setNome] = useState('Nome indefinido')
+    const [idade, setIdade] = useState(0)
+
     // Função será passada como props pro filho
-    function receberInfo(nome, idade){
-        console.log(nome, idade)
+    function receberInfo(nome, idade) {
+        setNome(nome)
+        setIdade(idade)
     }
 
     return (
         <div>
             <div>Pai</div>
-            <IndiretoFilho funcaoDoPai={receberInfo}/>
+            <IndiretoFilho funcaoDoPai={receberInfo} />
+            <p>Nome: {nome}</p>
+            <p>Idade: {idade}</p>
         </div>
     )
 }
