9263b096bc97895cb6b25eacf01f61fdaa2ca627
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2021-03-01 18:03:32 -0300

    props dos botoes

--- CursoUdemy/calculadora/src/components/Button.jsx
+++ CursoUdemy/calculadora/src/components/Button.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
 import './Button.css'
 
-const Button = props => <button className='button'>{props.label}</button>
+
+const Button = props => {
+
+    let estilo = 'button '
+    estilo += props.operation ? 'operation ' : ''
+    estilo += props.double ? 'double ' : ''
+    estilo += props.triple ? 'triple ' : ''
+    
+    return (
+        
+        <button className={estilo} onClick={e => props.funcao(props.label)}>
+            {props.label}
+        </button>
+    )
+    
+}
 
 export default Button
+
+/*  VARIÁVEL ESTILO
+
+    Irá verifica uma propriedade do parent compoente
+    Casso essa propriedade exista ira aplica-la
+    junto ao estilo padrão (button) 
+*/
--- CursoUdemy/calculadora/src/main/Calculator.jsx
+++ CursoUdemy/calculadora/src/main/Calculator.jsx
@@ -6,28 +6,51 @@ import Display from '../components/Display'
 
 class Calculator extends React.Component {
     render() {
+
+        const clearMemori = () => console.log('limpar')
+        const setOperation = op => console.log(op)
+        const addDigito = digito => console.log(digito)
+
         return (
             <div className='calculator'>
-                <Display value='100' />
-                <Button label='AC' />
-                <Button label='/' />
-                <Button label='7' />
-                <Button label='8' />
-                <Button label='9' />
-                <Button label='AC' />
-                <Button label='4' />
-                <Button label='5' />
-                <Button label='6' />
-                <Button label='-' />
-                <Button label='1' />
-                <Button label='2' />
-                <Button label='3' />
-                <Button label='+' />
-                <Button label='0' />
-                <Button label='.' />
-                <Button label='=' />
+                <>
+                    <Display value='100' />
+                </>
+                <>
+                    <Button label='AC' funcao={clearMemori} triple />
+                    <Button label='/' funcao={setOperation} operation />
+                    <Button label='7' funcao={addDigito} />
+                    <Button label='8' funcao={addDigito} />
+                    <Button label='9' funcao={addDigito} />
+                    <Button label='*' funcao={setOperation} operation />
+                    <Button label='4' funcao={addDigito} />
+                    <Button label='5' funcao={addDigito} />
+                    <Button label='6' funcao={addDigito} />
+                    <Button label='-' funcao={setOperation} operation />
+                    <Button label='1' funcao={addDigito} />
+                    <Button label='2' funcao={addDigito} />
+                    <Button label='3' funcao={addDigito} />
+                    <Button label='+' funcao={setOperation} operation />
+                    <Button label='0' funcao={addDigito} double/>
+                    <Button label='.' funcao={addDigito} />
+                    <Button label='=' funcao={setOperation} operation/>
+                </>
             </div>
         )
     }
 }
 export default Calculator
+
+/*  PROPRIEDADES NOS BOTOES
+
+    label: valor a ser mostrado no Componente Display
+    funcao: irá passar uma das tres função conforme o btn
+    tiple double ou operation: propriedade que será usada para definir estilo btn
+*/
+
+/* FUNÇÕES
+
+    clearMemory: limpara o display
+    setOperation: passará umas das operações par o display (+ - * /)
+    addDigito: passará o valor do label para o display
+ */
