d92f02b708bc7f9d4aaca35ccc15c2fe07f44262
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2021-03-01 19:57:40 -0300

    Finalizando calculadora

--- CursoUdemy/calculadora/src/main/Calculator.jsx
+++ CursoUdemy/calculadora/src/main/Calculator.jsx
@@ -4,17 +4,78 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 class Calculator extends React.Component {
+
+    state = { ...initialState }
+
     render() {
 
-        const clearMemori = () => console.log('limpar')
-        const setOperation = op => console.log(op)
-        const addDigito = digito => console.log(digito)
+        const clearMemori = () => this.setState({ ...initialState })
+
+        const setOperation = operation => {
+            if (this.state.current === 0) {
+                this.setState({ operation, current: 1, clearDisplay: true })
+            } else {
+                const equals = operation === '='
+                const currentOperation = this.state.operation
+
+                const values = [...this.state.values]
+                try {
+                    values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+                } catch (e) {
+                    values[0] = this.state.values[0]
+                }
+                values[1] = 0
+
+                this.setState({
+                    displayValue: values[0],
+                    operation: equals ? null : operation,
+                    current: equals ? 0 : 1,
+                    clearDisplay: !equals,
+                    values
+                })
+            }
+        }
+
+        const addDigito = n => {
+
+            // Verefica se ponto já foi digitado, se sim saira sem fazer nada
+            if (n === '.' && this.state.displayValue.includes('.')) {
+                return
+            }
+
+            // Caso so tenha zero no display limpara para não acumular zeros desnecessarios
+            const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+            const currentValue = clearDisplay ? '' : this.state.displayValue
+            const displayValue = currentValue + n
+
+            this.setState({ displayValue, clearDisplay: false })
+
+            if (n !== '.') {
+                const i = this.state.current
+                const newValue = parseFloat(displayValue)
+                const values = [...this.state.values]
+                values[i] = newValue
+                this.setState({ values })
+
+                console.log(values)
+
+            }
+
+        }
 
         return (
             <div className='calculator'>
                 <>
-                    <Display value='100' />
+                    <Display value={this.state.displayValue} />
                 </>
                 <>
                     <Button label='AC' funcao={clearMemori} triple />
@@ -31,9 +92,9 @@ class Calculator extends React.Component {
                     <Button label='2' funcao={addDigito} />
                     <Button label='3' funcao={addDigito} />
                     <Button label='+' funcao={setOperation} operation />
-                    <Button label='0' funcao={addDigito} double/>
+                    <Button label='0' funcao={addDigito} double />
                     <Button label='.' funcao={addDigito} />
-                    <Button label='=' funcao={setOperation} operation/>
+                    <Button label='=' funcao={setOperation} operation />
                 </>
             </div>
         )
